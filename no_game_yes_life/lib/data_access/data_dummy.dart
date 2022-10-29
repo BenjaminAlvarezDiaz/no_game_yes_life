f51b8b67d8e143f78aab7b561cffb7343c3445a9
BenjaminAlvarezDiaz <benja77744@gmail.com>
2022-10-29 05:29:36 -0300

    Cambiando el modelo de DailyTask
    Ahora DailyTask toma la frecuencia con la que el usuario interactúa con una u otra task. Agregado también a los datos Dummy.

--- no_game_yes_life/lib/data_access/data_dummy.dart
+++ no_game_yes_life/lib/data_access/data_dummy.dart
@@ -1,10 +1,16 @@
 import 'package:no_game_yes_life/interfaces/idata_access.dart';
+import 'package:no_game_yes_life/src/models/daily_task_model.dart';
 import 'package:no_game_yes_life/src/models/hobby_stop_watch_model.dart';
 import 'package:no_game_yes_life/src/models/user_model.dart';
 
 class Dummy implements IDataAccess{
   late HobbyStopWatch hobbyStopWatch;
   late User user;
+  late DailyTask dailyTask;
+
+  init(){
+    dailyTask = DailyTask(0, 0, 0);
+  }
 
   @override
   getCountDown() {
@@ -19,6 +25,21 @@ class Dummy implements IDataAccess{
   }
 
   @override
+  getAmountEasy(){
+    return dailyTask.amountEasy;
+  }
+
+  @override
+  getAmountNormal(){
+    return dailyTask.amountNormal; //todos los getAmount son parte de la frecuencia con la que se hace una u otra tarea
+  }
+
+  @override
+  getAmountHard(){
+    return dailyTask.amountHard;
+  }
+
+  @override
   setNickname(String newNickname){
     //print(user.name);
     user.name = newNickname;
@@ -29,5 +50,20 @@ class Dummy implements IDataAccess{
   setRoutine(double newRoutine){
     user.routine = newRoutine;
     print(newRoutine);
+  }
+
+  @override
+  setAmountEasy(int newAmountEasy){
+    dailyTask.amountEasy = newAmountEasy;
+  }
+
+  @override
+  setAmountNormal(int newAmountNormal){
+    dailyTask.amountNormal = newAmountNormal;
+  }
+
+  @override
+  setAmountHard(int newAmountHard){
+    dailyTask.amountHard = newAmountHard;
   }
 }
--- no_game_yes_life/lib/src/models/daily_task_model.dart
+++ no_game_yes_life/lib/src/models/daily_task_model.dart
@@ -1,12 +1,29 @@
 class DailyTask{
 
-  late Duration timeEasy;
-  late Duration timeNormal;
-  late Duration timeHard;
+  late int _amountEasy;
+  late int _amountNormal;
+  late int _amountHard;
 
-  DailyTask(_timeEasy, _timeNormal, _timeHard){
-    timeEasy = _timeEasy;
-    timeNormal = _timeNormal;
-    timeHard = _timeHard;
+  DailyTask(timeEasy, timeNormal, timeHard){
+    _amountEasy = timeEasy;
+    _amountNormal = timeNormal;
+    _amountHard = timeHard;
   }
+
+  int get amountEasy => _amountEasy;
+  int get amountNormal => _amountNormal;
+  int get amountHard => _amountHard;
+
+  set amountEasy(int value){
+    _amountEasy = value;
+  }
+
+  set amountNormal(int value){
+    _amountNormal = value;
+  }
+
+  set amountHard(int value) {
+    _amountHard = value;
+  }
+
 }
